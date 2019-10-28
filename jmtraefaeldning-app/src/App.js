b6fc8aeff5ee71fa12e9934c4c7caa4f3aed05e5
emil <emij@itu.dk>
2019-10-28 20:59:00 +0100

    making nav bar

--- jmtraefaeldning-app/src/App.js
+++ jmtraefaeldning-app/src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import Imgcarousel from './components/Imgcarousel'
+import React, {Component} from 'react';
+import Nav from './components/Nav'
 
-
-function App() {
+class App extends Component {
+ state = {
+      nav: [
+        {
+          id: 1,
+          title: 'Hjem',
+          Marked: false
+        },
+        {
+          id: 1,
+          title: 'Privat',
+          Marked: false
+        },
+        {
+          id: 1,
+          title: 'Erhverv',
+          Marked: false
+        },
+        {
+          id: 1,
+          title: 'Kontakt',
+          Marked: false
+        },
+      ]
+ };
+ render(){
+ console.log(this.state.nav)
   return (
     <div className="App">
-      <h1>TEST</h1>
-      <Imgcarousel />
+      <Nav />
+
     </div>
   );
+ }
 }
 
 export default App;
---
+++ jmtraefaeldning-app/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Nav extends Component {
+ render(){
+ return (
+    <div>
+      <h1>TEST</h1>
+    </div>
+  );
+ }
+}
+
+export default Nav;
