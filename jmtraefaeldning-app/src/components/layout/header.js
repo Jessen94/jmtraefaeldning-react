ed5bdb6d9aa56ac62199b8f1dc938b77f48cab53
emil <emij@itu.dk>
2019-11-01 18:22:53 +0100

    fixed nav bar

--- jmtraefaeldning-app/src/components/layout/header.js
+++ jmtraefaeldning-app/src/components/layout/header.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class header extends Component {
     render() {
         return (
-            <div class="container">
+            <div class="header">
                 <nav>
                     <ul>
                         <li id="Home"><p>Hjem</p></li>
                         <li id="Business"><p>Erhverv</p></li>
                         <li id="Private"><p>Privat</p></li>
-                        <li id="Contact">Kontakt</li>
+                        <li id="Contact"><p>Kontakt</p></li>
                     </ul>
                 </nav>
             </div>
