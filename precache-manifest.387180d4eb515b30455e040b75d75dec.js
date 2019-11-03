e7d00db50f478013949591db9406995e81b044d9
emil <emij@itu.dk>
2019-11-03 13:54:39 +0100

    Updates

---
+++ precache-manifest.387180d4eb515b30455e040b75d75dec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aef1fcd510b8b06d991ce88b348b591",
+    "url": "/JMtraefaeldning-react/index.html"
+  },
+  {
+    "revision": "54e4a9fe27fe68421a05",
+    "url": "/JMtraefaeldning-react/static/css/main.29ab1373.chunk.css"
+  },
+  {
+    "revision": "ca2f53eaf035dfbe1bf5",
+    "url": "/JMtraefaeldning-react/static/js/2.d12b61b8.chunk.js"
+  },
+  {
+    "revision": "54e4a9fe27fe68421a05",
+    "url": "/JMtraefaeldning-react/static/js/main.081cd731.chunk.js"
+  },
+  {
+    "revision": "369446fa9ef81bfe22f2",
+    "url": "/JMtraefaeldning-react/static/js/runtime-main.9f9caee1.js"
+  }
+]);
