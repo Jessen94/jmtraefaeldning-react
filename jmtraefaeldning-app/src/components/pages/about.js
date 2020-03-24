34a0ee68c084ff079fcd8e4b6f2358f415b822d4
emilj <emil.jessen94@gmail.com>
2020-03-24 17:32:44 +0100

    Styling fixes

--- jmtraefaeldning-app/src/components/pages/about.js
+++ jmtraefaeldning-app/src/components/pages/about.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 function about() {
     return (
-        <div className="about--container">
-            <ul>
-                <li><h1>Email:  JMTraefaeldning@gmail.com </h1></li><br />
-                <li><h1>Tlf:  22289789 </h1></li><br />
-                <li><h1>CVR:  40691448 </h1></li><br />
-                <li><h1>Adresse:  Blåmejsevej 27,  2600 Glostrup </h1></li><br />
-            </ul>
-        </div>
+        <>
+            <div className="home-headline">
+                <h1>Kontakt</h1>
+            </div>
+            <div className="about--container">
+                <ul>
+                    <li><h1>Email:  JMTraefaeldning@gmail.com </h1></li><br />
+                    <li><h1>Tlf:  22289789 </h1></li><br />
+                    <li><h1>CVR:  40691448 </h1></li><br />
+                    <li><h1>Adresse:  Blåmejsevej 27,  2600 Glostrup </h1></li><br />
+                </ul>
+            </div>
+        </>
     )
 }
 
--- jmtraefaeldning-app/src/components/pages/home.js
+++ jmtraefaeldning-app/src/components/pages/home.js
@@ -11,11 +11,13 @@ function home() {
                 </div>
                 <div className="intro">
                     <div className="intro--text">
+                    <div className="intro-text-headline">
                         <h1>Professionelt og effektivt</h1>
+                    </div>
                         <p>
                             JMtræfældning er drevet af mig Jens Moesgaard. Fældning foregår altid med hensyn til de omgivelser som træet, eller træerne befinder sig i.<br /><br /><br />
                             Oprydningen klare jeg også gerne for dig, du bestemmer helt selv om det skal skæres op til brænde eller om jeg skal tage det med.
-                     </p>
+                        </p>
                         </div>
                         <img className="img--intro" src={img} alt="Jens posere med motorsav" />
                     </div>
